841c523daa0f0a769f260462c191e80cde746122
soowan <tndhks0708@naver.com>
2026-02-24 15:12:06 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 1.23 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,5 +1,5 @@
 /*
-비트마스크
+비트마스크. beginning[i][j] ^ row[r] ^ col[c] == target[i][j]
 1. 모든 행 뒤집기 조합 탐색
 2. bitCount로 뒤집은 행 개수 계산
 3. 첫 행 기준으로 열 뒤집기 자동 결정
@@ -15,18 +15,17 @@ class Solution {
         int r = target.length;
         int c = target[0].length;
         
-        for (int mask = 0; mask < (1<<r); mask++) {
+        for (int mask = 0; mask < (1 << r); mask++) {
             // 뒤집은 행 개수 계산. 1이면 뒤집고 0이면 그대로
             int rowCount = Integer.bitCount(mask);
             boolean possible = true;
             
-            int[] col = new int[c];
-            
             // 열 뒤집기 결정 (첫 행 기준)
+            int[] col = new int[c];
             for (int j = 0; j < c; j++) col[j] = beginning[0][j] ^ target[0][j] ^ (mask & 1);
             
-            // 검증
-            for (int i = 0; i < r && possible; i++) {
+            // 열 뒤집기 검증
+            for (int i = 1; i < r && possible; i++) {
                 for (int j = 0; j < c; j++) {
                     int value = beginning[i][j] ^ col[j] ^ ((mask >> i) & 1);
                     
