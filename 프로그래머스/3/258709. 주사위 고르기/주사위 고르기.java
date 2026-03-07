98103e9426e9b996109b013e1c931e19ee83852c
soowan <tndhks0708@naver.com>
2026-03-07 17:45:05 +0900

    [level 3] Title: 주사위 고르기, Time: 297.47 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,6 +1,8 @@
 /*
-1. 조합 구하기 (dfs)
-2. 승리할 확률이 크면 해당 조합 저장
+dfs (조합, 합 생성), 이분탐색 (승리 수 계산)
+1. 
+2. 
+3. 
 */
 
 import java.util.*;
@@ -16,32 +18,27 @@ class Solution {
         answer = new int[n / 2];
         
         // 조합 구하기 (dfs)
-        boolean[] visited = new boolean[n];
-        List<Integer> listA = new ArrayList<>();
-        
-        getCombination(dice, visited, listA, 0);
+        getCombination(dice, new boolean[n], new ArrayList<>(), 0);
         
         return answer;
     }
     
+    // 조합
     void getCombination(int[][] dice, boolean[] visited, List<Integer> listA, int start) {
         if (listA.size() == n / 2) {
             // listB 구하기
             List<Integer> listB = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                if (!visited[i]) listB.add(i);
-            }
+            for (int i = 0; i < n; i++) if (!visited[i]) listB.add(i);
             
             int win = countWin(dice, listA, listB);
             
             if (win > count) {
                 count = win;
-                for (int i = 0; i < listA.size(); i++) {
-                    answer[i] = listA.get(i) + 1;
-                }
+                for (int i = 0; i < listA.size(); i++) answer[i] = listA.get(i) + 1;
             }
         }
         
+        // dfs
         for (int i = start; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
@@ -55,9 +52,10 @@ class Solution {
         }
     }
     
+    // 승리 횟수
     int countWin(int[][] dice, List<Integer> listA, List<Integer> listB) {
         int len = listA.size();
-            
+        
         int[] arrA = new int[(int) Math.pow(6, len)];
         int[] arrB = new int[(int) Math.pow(6, len)];
         
@@ -70,18 +68,27 @@ class Solution {
         Arrays.sort(arrA);
         Arrays.sort(arrB);
         
+        // 승리 횟수 구하기. 이분탐색
         int win = 0;
         
         for (int a : arrA) {
-            for (int b : arrB) {
-                if (a > b) win++;
-                else break;
+            int start = 0;
+            int end = arrB.length - 1;
+            
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                
+                if (arrB[mid] < a) start = mid + 1;
+                else end = mid - 1;
             }
+            
+            win += start;
         }
         
         return win;
     }
     
+    // 합 구하기
     void dfs(int[] arr, int[][] dice, List<Integer> list, int depth, int sum) {
         if (depth == list.size()) {
             arr[idx++] = sum;
@@ -90,8 +97,6 @@ class Solution {
         
         int diceIndex = list.get(depth);
         
-        for (int i = 0; i < 6; i++) {
-            dfs(arr, dice, list, depth + 1, sum + dice[diceIndex][i]);
-        }
+        for (int i = 0; i < 6; i++) dfs(arr, dice, list, depth + 1, sum + dice[diceIndex][i]);
     }
 }
