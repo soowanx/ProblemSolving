d779292769636e2e42728a41a1b9c262bcbed463
soowan <tndhks0708@naver.com>
2026-04-06 14:35:14 +0900

    [Gold V] Title: 토마토, Time: 620 ms, Memory: 126236 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -2,9 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, 1, -1};
-    static int[] dh = {-1, 1};
+    static int[] dr = {1, -1, 0, 0, 0, 0};
+    static int[] dc = {0, 0, 1, -1, 0, 0};
+    static int[] dh = {0, 0, 0, 0, 1, -1};
 
     static int solution (int n, int m, int h, int[][][] arr) {
         Queue<int[]> queue = new LinkedList<>();
@@ -13,13 +13,11 @@ public class Main {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < m; k++) {
                     if (arr[i][j][k] == 1) {
-                        queue.add(new int[] {i, j, k, 0});
+                        queue.add(new int[] {i, j, k});
                     }
                 }
             }
         }
-
-        int time = 0;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
@@ -27,40 +25,36 @@ public class Main {
             int height = cur[0];
             int r = cur[1];
             int c = cur[2];
-            time = cur[3];
 
-            // 상하좌우
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 6; i++) {
+                int nh = height+ dh[i];
                 int nr = r + dr[i];
                 int nc = c + dc[i];
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && arr[height][nr][nc] == 0) {
-                    arr[height][nr][nc] = 1;
-                    queue.add(new int[] {height, nr, nc, time + 1});
-                }
-            }
-
-            // 위아래
-            for (int i = 0; i < 2; i++) {
-                int nh = height + dh[i];
-
-                if (nh >= 0 && nh < h && arr[nh][r][c] == 0) {
-                    arr[nh][r][c] = 1;
-                    queue.add(new int[] {nh, r, c, time + 1});
+                if (nh >= 0 & nh < h && nr >= 0 && nr < n && nc >= 0 && nc < m && arr[nh][nr][nc] == 0) {
+                    arr[nh][nr][nc] = arr[height][r][c] + 1;
+                    queue.add(new int[] {nh, nr, nc});
                 }
             }
         }
 
-        // 모두 익지 못하는 상황
+        int max = -1;
+
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < m; k++) {
-                    if (arr[i][j][k] == 0) return -1;
+                    if (max < arr[i][j][k]) {
+                        max = arr[i][j][k];
+                    }
+
+                    if (arr[i][j][k] == 0) {
+                        return -1;
+                    }
                 }
             }
         }
 
-        return time;
+        return max - 1;
     }
 
     public static void main(String[] args) throws IOException {
