fcaf18fc0ed3d2dc50693087bb8cfb6abfa4f4b0
soowan <tndhks0708@naver.com>
2026-04-09 22:57:34 +0900

    [Gold IV] Title: 최단경로, Time: 672 ms, Memory: 121908 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class Main {
     static String solution (int vCount, int eCount, int start, int[][] edges) {
+        // 그래프 초기화
         List<List<int[]>> graph = new ArrayList<>();
 
         for (int i = 0; i <= vCount; i++) {
@@ -10,28 +11,28 @@ public class Main {
         }
 
         for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-
-            graph.get(u).add(new int[] {v, w});
+            graph.get(edge[0]).add(new int[] {edge[1], edge[2]});
         }
 
+        // dist 배열 초기화
         int[] dist = new int[vCount + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
 
-
+        // 우선순위 큐: 비용 낮은 순
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
             return Integer.compare(a[1], b[1]);
         });
 
         queue.add(new int[] {start, 0});
 
+        // 다익스트라
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int curNum = cur[0];
             int curCost = cur[1];
+
+            if (curCost > dist[curNum]) continue;
 
             for (int[] next : graph.get(curNum)) {
                 int nextNum = next[0];
@@ -45,7 +46,6 @@ public class Main {
                 }
             }
         }
-
 
         StringBuilder sb = new StringBuilder();
 
@@ -63,21 +63,16 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int vCount = Integer.parseInt(st.nextToken());
         int eCount = Integer.parseInt(st.nextToken());
+        int[][] edges = new int[eCount][3];
 
         int k = Integer.parseInt(br.readLine());
-
-        int[][] edges = new int[eCount][3];
 
         for (int i = 0; i < eCount; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            edges[i][0] = u;
-            edges[i][1] = v;
-            edges[i][2] = w;
+            edges[i][0] = Integer.parseInt(st.nextToken());
+            edges[i][1] = Integer.parseInt(st.nextToken());
+            edges[i][2] = Integer.parseInt(st.nextToken());
         }
 
         System.out.println(solution(vCount, eCount, k, edges));
