8320259ad667c8d239fe7c6b96f0b50381bd5536
soowan <tndhks0708@naver.com>
2026-04-10 18:24:34 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 220 ms, Memory: 29248 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -5,12 +5,17 @@ public class Main {
     static int[] dp = {-1, 1};
 
     static int solution(int n, int k) {
-        boolean[] visited = new boolean[500_000];
-        visited[n] = true;
+        // dist 배열
+        int[] dist = new int[500000];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[n] = 0;
 
         // 우선순위 큐: 비용 낮은 순
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.addFirst(new int[] {n, 0});
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            return Integer.compare(a[1], b[1]);
+        });
+
+        queue.add(new int[] {n, 0});
 
         // 다익스트라 + 지나온 경로 저장
         while (!queue.isEmpty()) {
@@ -23,16 +28,16 @@ public class Main {
             }
 
             int next = pos * 2;
-            if (next <= 200000 && !visited[next]) {
-                visited[next] = true;
-                queue.addFirst(new int[] {next, time});
+            if (next <= 200000 && time < dist[next]) {
+                dist[next] = time;
+                queue.add(new int[] {next, time});
             }
 
             for (int d : dp) {
                 next = pos + d;
-                if (next >= 0 && next <= 200000 && !visited[next]) {
-                    visited[next] = true;
-                    queue.addLast(new int[] {next, time + 1});
+                if (next >= 0 && time + 1 < dist[next]) {
+                    dist[next] = time + 1;
+                    queue.add(new int[] {next, time + 1});
                 }
             }
         }
