428a801137fe59178679517b6af2b4a88affbbad
soowan <tndhks0708@naver.com>
2026-04-11 15:38:02 +0900

    [Gold IV] Title: 운동, Time: 636 ms, Memory: 63940 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -2,82 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dijkstra(List<List<int[]>> graph, int[] dist, int start) {
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
-            return Integer.compare(a[1], b[1]);
-        });
-
-        queue.add(new int[] {start, 0});
-
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int num = cur[0];
-            int d = cur[1];
-
-            if (dist[num] < d) {
-                continue;
-            }
-
-            for (int[] next : graph.get(num)) {
-                int nextNum = next[0];
-                int nextD = next[1];
-
-                int value = d + nextD;
-
-                if (value < dist[nextNum]) {
-                    dist[nextNum] = value;
-                    queue.add(new int[] {nextNum, value});
-                }
-            }
-        }
-
-        return dist;
-    }
+    static int MAX = Integer.MAX_VALUE;
 
     static void solution(int v, int[][] edges) {
-        // 그래프 만들기
-        List<List<int[]>> graph = new ArrayList<>();
-
-        for (int i = 0; i <= v; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int[] edge : edges) {
-            graph.get(edge[0]).add(new int[] {edge[1], edge[2]});
-        }
-
         // dist 배열
         int[][] dist = new int[v + 1][v + 1];
 
         for (int i = 0; i <= v; i++) {
             for (int j = 0; j <= v; j++) {
-                dist[i][j] = i == j ? 0 : Integer.MAX_VALUE;
+                dist[i][j] = MAX;
             }
         }
 
-        // 다익스트라
-        for (int i = 1; i <= v; i++) {
-            dist[i] = dijkstra(graph, dist[i], i);
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            int c = edge[2];
+
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        // 플로이드 워셜
+        for (int k = 1; k <= v; k++) {
+            for (int i = 1; i <= v; i++) {
+                for (int j = 1; j <= v; j++) {
+                    if (dist[i][k] == MAX || dist[k][j] == MAX) {
+                        continue;
+                    }
+
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
         }
 
         // return
-        int min = Integer.MAX_VALUE;
+        int min = MAX;
 
-        for (int i = 1; i < v; i++) {
-            for (int j = i + 1; j <= v; j++) {
-                if (dist[i][j] == Integer.MAX_VALUE || dist[j][i] == Integer.MAX_VALUE) {
-                    continue;
-                }
-
-                int d = dist[i][j] + dist[j][i];
-
-                if (d < min) {
-                    min = d;
-                }
+        for (int i = 1; i <= v; i++) {
+            if (dist[i][i] == MAX) {
+                continue;
             }
+
+            min = Integer.min(min, dist[i][i]);
         }
 
-        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+        System.out.println(min == MAX ? -1 : min);
     }
 
     public static void main(String[] args) throws IOException {
