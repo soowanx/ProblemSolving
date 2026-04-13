1177bf979178ebb2bafb1e9f173e8c8fbf40de31
soowan <tndhks0708@naver.com>
2026-04-13 19:42:06 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,63 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-class Main {
+public class Main {
+    static void solution(int n, int[] arr) {
+        int[][] dp = new int[2][n];
+
+        // 앞 -> 뒤
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[0][i] = Math.max(dp[0][i], dp[0][j] + 1);
+                }
+            }
+        }
+
+        // 뒤 -> 앞
+        dp[1][n - 1] = 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            dp[1][i] = 1;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[1][i] = Math.max(dp[1][i], dp[1][j] + 1);
+                }
+            }
+        }
+
+        // 출력
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[0][i] + dp[1][i]);
+        }
+
+        System.out.println(max - 1);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-
         int[] arr = new int[n];
-        int[][] dp = new int[3][n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // n == 1
-        if (n == 1) {
-            System.out.println(1);
-            return;
-        }
-
-        // dp[0][]: 앞에서 증가
-        dp[0][0] = 1;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j] && dp[0][i] < dp[0][j]) {
-                    dp[0][i] = dp[0][j];
-                }
-            }
-            dp[0][i] += 1;
-        }
-
-        // dp[1][]: 뒤에서 증가
-        dp[1][n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] > arr[j] && dp[1][i] < dp[1][j]) {
-                    dp[1][i] = dp[1][j];
-                }
-            }
-            dp[1][i] += 1;
-        }
-
-        // dp[2][] = dp[0][] + dp[1][]
-        for (int i = 0; i < n; i++) {
-            dp[2][i] = dp[0][i] + dp[1][i];
-        }
-
-        // 출력
-        int max = -1;
-        for (int i = 0; i < n; i++) {
-            if (max < dp[2][i]) {
-                max = dp[2][i];
-            }
-        }
-        System.out.print(max - 1);
+        solution(n, arr);
     }
 }
