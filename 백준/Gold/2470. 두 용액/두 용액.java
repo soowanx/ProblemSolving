d36ad2f39d824746e78218fbd583525cfa8afb13
soowan <tndhks0708@naver.com>
2026-04-13 18:14:40 +0900

    [Gold V] Title: 두 용액, Time: 336 ms, Memory: 28120 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static void solution(int[] arr) {
+        Arrays.sort(arr);
+
+        int[] answer = new int[2];
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        int min = Integer.MAX_VALUE;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                answer[0] = arr[left];
+                answer[1] = arr[right];
+            }
+
+            if (sum < 0) {
+                left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                break;
+            }
+        }
+
+        Arrays.sort(answer);
+        StringBuilder sb = new StringBuilder();
+        sb.append(answer[0]).append(" ").append(answer[1]);
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solution(arr);
+    }
+}
