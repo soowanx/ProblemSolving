92759e8b69afcab0607f63089068af877f3f4d60
soowan <tndhks0708@naver.com>
2026-04-26 17:25:25 +0900

    [level 3] Title: 사라지는 발판, Time: 17.06 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -0,0 +1,50 @@
+class Solution {
+    int[] dr = {1, -1, 0, 0};
+    int[] dc = {0, 0, 1, -1};
+    
+    int n;
+    int m;
+    
+    int[] dfs(int[][] board, int cr, int cc, int or, int oc) {
+        if (board[cr][cc] == 0) {
+            return new int[] {0, 0};
+        }
+        
+        board[cr][cc] = 0;
+        
+        int minWin = Integer.MAX_VALUE;
+        int maxLose = 0;
+        
+        boolean canWin = false;
+        
+        for (int i = 0; i < 4; i++) {
+            int nr = cr + dr[i];
+            int nc = cc + dc[i];
+            
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m || board[nr][nc] == 0) {
+                continue;
+            }
+            
+            int[] res = dfs(board, or, oc, nr, nc);
+            
+            if (res[0] == 0) { // 상대 패배
+                canWin = true;
+                minWin = Math.min(minWin, res[1] + 1);
+            } else { // 상대 승리
+                maxLose = Math.max(maxLose, res[1] + 1);
+            }
+        }
+        
+        board[cr][cc] = 1;
+        
+        return canWin ? new int[] {1, minWin} : new int[] {0, maxLose};
+    }
+    
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        n = board.length;
+        m = board[0].length;
+        
+        int[] answer = dfs(board, aloc[0], aloc[1], bloc[0], bloc[1]);
+        return answer[1];
+    }
+}
